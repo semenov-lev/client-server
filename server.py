3a4a644b6e0e278122c10e25db17045f9cc7d564
semenov-lev <firemail.leo@gmail.com>
2022-04-22 12:50:39 +0300

    completed

--- server.py
+++ server.py
@@ -1,14 +1,38 @@
-import socket
 import sys
+import time
+from ipaddress import ip_address
 from socket import socket, AF_INET, SOCK_STREAM
-import ipaddress
-import yaml
-import json
+
+import common.variables as variables
+from common.utils import encode_message, decode_message
 
 
-def get_variables():
-    with open("common/settings.yaml", "r") as f_n:
-        return yaml.safe_load(f_n)
+def message_handler(message):
+    action = variables.PRESENCE
+    account_name = variables.ACCOUNT_NAME
+    timestamp = int(time.time())
+
+    try:
+
+        if message["action"] == action and message["time"] and message["user"]["account_name"] == account_name:
+
+            message = {"response": "200",
+                       "time": timestamp}
+
+        elif message["action"] == action and message["time"] and message["user"]["account_name"] != account_name:
+
+            message = {"response": "404",
+                       "time": timestamp,
+                       "alert": "Пользователь/чат отсутствует на сервере"}
+        else:
+            raise KeyError
+
+    except KeyError:
+        message = {"response": "400",
+                   "time": timestamp,
+                   "alert": "Неправильный запрос/JSON-объект"}
+
+    return encode_message(message)
 
 
 def main():
@@ -17,15 +41,13 @@ def main():
     :return:
     """
 
-    VARIABLES = get_variables()
-
     try:
         if "-a" in sys.argv:
-            address = ipaddress.ip_address(sys.argv[sys.argv.index("-a") + 1])
+            address = str(ip_address(sys.argv[sys.argv.index("-a") + 1]))
         else:
             address = ''
     except ValueError:
-        print(f'Некорректно введен адрес')
+        print("Некорректно введен адрес")
         sys.exit(1)
 
     try:
@@ -34,18 +56,29 @@ def main():
             if 1024 > port > 65535:
                 raise ValueError
         else:
-            port = VARIABLES["DEFAULT_PORT"]
+            port = variables.DEFAULT_PORT
     except ValueError:
         print("Значение <port> должно быть числом, в диапазоне с 1024 по 65535")
         sys.exit(1)
 
     s = socket(AF_INET, SOCK_STREAM)
     s.bind((address, port))
-    s.listen(VARIABLES["MAX_CONNECTIONS"])
+    s.listen(variables.MAX_CONNECTIONS)
 
     while True:
         client, client_address = s.accept()
-        data = client.recv(VARIABLES["MAX_PACKAGE_LENGTH"])
+
+        received_data = client.recv(variables.MAX_PACKAGE_LENGTH)
+
+        received_message = decode_message(received_data)
+
+        print(f"Сообщение от клиента: {received_message}")
+
+        response_data = message_handler(received_message)
+
+        client.send(response_data)
+
+        client.close()
 
 
 if __name__ == "__main__":
