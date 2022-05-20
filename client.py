fcb071fc0748fe931b9853b9b9b44cda44b5416c
semenov-lev <firemail.leo@gmail.com>
2022-05-20 16:40:54 +0300

    added P2P for client, server-part in progress

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import time
 import logging
+import threading
 from ipaddress import ip_address
 from socket import socket, AF_INET, SOCK_STREAM
 
@@ -18,12 +19,10 @@ def arg_parser():
     parser = argparse.ArgumentParser(description='Client script')
     parser.add_argument('-a', dest='address', default='')
     parser.add_argument('-p', dest='port', default=variables.DEFAULT_PORT, type=int)
-    parser.add_argument('-m', dest='mode', default="listen")
     args = parser.parse_args()
     address = args.address
-    mode = args.mode
     port = args.port
-    return address, port, mode
+    return address, port
 
 
 @log
@@ -44,98 +43,118 @@ def presence_message(account_name):
 
 
 @log
-def message_to_all(message, account_name):
+def response_handler(response):
+    if response["response"] == "200":
+        print(f"Сообщение от сервера: {response}")
+        CLIENT_LOGGER.info(f"Response: 200, ОК")
+    else:
+        print(f"Сообщение от сервера: {response}")
+        CLIENT_LOGGER.warning(f"Response: {response}!!!")
+
+
+@log
+def user_interaction(sock, account_name):
+    while True:
+        dest = str(input("Имя получателя, или '/q' для выхода: "))
+        if dest == '/q':
+            sock.close()
+            sys.exit(0)
+        msg = str(input(f"Cообщение для {dest}: "))
+
+        message_data = encode_message(send_message(msg, account_name, dest))
+        sock.send(message_data)
+
+
+@log
+def send_message(message, account_name, dest):
     timestamp = int(time.time())
 
     return {
         "action": "msg",
         "time": timestamp,
-        "to": "#all",
+        "to": dest,
         "from": account_name,
         "message": message
     }
 
 
 @log
-def message_handler(message):
-    if "action" in message and message["action"] == "msg":
-        print(f"Сообщение от пользователя {message['from']}:\n"
-              f"{message['message']}")
-        CLIENT_LOGGER.info(f"Сообщение от пользователя {message['from']}: {message['message']}")
-    else:
-        if message["response"] == "200":
-            print(f"Сообщение от сервера: {message}")
-            CLIENT_LOGGER.info(f"Response: 200, ОК")
-        else:
-            print(f"Сообщение от сервера: {message}")
-            CLIENT_LOGGER.warning(f"Response: {message}!!!")
+def receive_handler(sock, account_name):
+    while True:
+        message = decode_data(sock.recv(variables.MAX_PACKAGE_LENGTH))
+
+        try:
+            # Сообщения пользователей
+            if "action" in message and "to" in message:
+                if message["action"] == "msg":
+                    if message["to"] == account_name or message["to"] == "#all":
+                        print(f"{message['from']}: {message['message']}")
+                CLIENT_LOGGER.info(f"Сообщение от пользователя {message['from']} для {message['to']}: {message['message']}")
+            else:
+                # Сообщение от сервера
+                response_handler(message)
+        except KeyError:
+            print("receive_handler не распознал ключ в ответе")
 
 
 def main():
     """
-    client.py -a <address> -p [<port>] -m [mode]
+    client.py -a <address> -p [<port>]
     :return:
     """
 
-    server_address, server_port, mode = arg_parser()
+    server_address, server_port = arg_parser()
 
     try:
         ip_address(server_address)
     except ValueError:
-        CLIENT_LOGGER.critical("Некорректно введен адрес")
+        CLIENT_LOGGER.critical("Некорректно введен адресc!")
         sys.exit(1)
 
     if 1024 > server_port or server_port > 65535:
         CLIENT_LOGGER.critical("Значение <port> должно быть числом, в диапазоне с 1024 по 65535")
         sys.exit(1)
 
-    if mode not in ('listen', 'sender'):
-        CLIENT_LOGGER.critical("Режим может быть только 'listen', или 'sender'")
-        sys.exit(1)
-
-    if mode == 'sender':
-        ACCOUNT_NAME = variables.SENDER_ACCOUNT_NAME
-    else:
-        ACCOUNT_NAME = variables.LISTEN_ACCOUNT_NAME
-
-    CLIENT_LOGGER.debug(f"Подключаемся к серверу в режиме '{mode}'")
+    CLIENT_LOGGER.debug(f"Подключаемся к серверу...")
 
     try:
-        s = socket(AF_INET, SOCK_STREAM)
-        s.connect((server_address, server_port))
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.connect((server_address, server_port))
 
         CLIENT_LOGGER.info(f"Отправляем сообщение о присутствии")
-        presence_data = encode_message(presence_message(ACCOUNT_NAME))
-        s.send(presence_data)
 
-        data = s.recv(variables.MAX_PACKAGE_LENGTH)
-        message_handler(decode_data(data))
+        ACCOUNT_NAME = str(input("Ввести имя аккаунта: "))
+
+        # Отправляем приветствие и разбираем отклик
+        presence_data = encode_message(presence_message(ACCOUNT_NAME))
+        server_socket.send(presence_data)
+
+        response_data = server_socket.recv(variables.MAX_PACKAGE_LENGTH)
+        response_handler(decode_data(response_data))
 
     except:
-        CLIENT_LOGGER.critical("Произошла неведомая ошибка")
+        CLIENT_LOGGER.critical("Ошибка соединения с сервером!")
         sys.exit(1)
 
+    # try:
+    # Ввод
+    ui = threading.Thread(target=user_interaction, args=(server_socket, ACCOUNT_NAME))
+    ui.daemon = True
+    ui.start()
+
+    # Вывод
+
+    recv = threading.Thread(target=receive_handler, args=(server_socket, ACCOUNT_NAME))
+    recv.daemon = True
+    recv.start()
+    # except:
+    #     CLIENT_LOGGER.debug(f"Соединение прервано")
+    #     sys.exit(1)
+
     while True:
-        if mode == 'sender':
-            try:
-                msg = input("Ввести сообщение, или '/q' для выхода: ")
-                if msg == '/q':
-                    s.close()
-                    sys.exit(0)
-
-                message_data = encode_message(message_to_all(msg, ACCOUNT_NAME))
-                s.send(message_data)
-            except:
-                CLIENT_LOGGER.debug(f"Соединение закрыто у режима {mode}")
-                sys.exit(1)
-
-        if mode == 'listen':
-            try:
-                receive_message = decode_data(s.recv(variables.MAX_PACKAGE_LENGTH))
-                message_handler(receive_message)
-            except:
-                CLIENT_LOGGER.debug(f"Соединение закрыто у режима {mode}")
-                sys.exit(1)
+        if ui.is_alive() and recv.is_alive():
+            continue
+        break
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sys
-import time
 import logging
 import argparse
+from time import time
 from select import select
 from ipaddress import ip_address
 from socket import socket, AF_INET, SOCK_STREAM
@@ -26,49 +26,60 @@ def arg_parser():
 
 
 @log
-def message_handler(message):
+def message_handler(message, client, messages, accounts):
     """
-    Функция принимает сообщение-словарь,
-    проверяет информацию и возвращает сообщение-словарь
+    Функция принимает и обрабатывает message - словарь,
+    регистрирует новых пользователей и добавляет кортеж "получатель, сообщение"
+    в список messages
 
     :param message
-    :return message
+    :param peername
+    :param messages
+    :param accounts
+    :return
     """
 
-    timestamp = int(time.time())
-    account_names = [variables.LISTEN_ACCOUNT_NAME, variables.SENDER_ACCOUNT_NAME]
+    # TODO: Доработать словарь, отправку по адресам
 
     try:
         SERVER_LOGGER.debug("Обработка сообщения от клиента")
+        if message["action"] == 'presence':
+            sender = message["user"]["account_name"]
 
-        if message["action"] == variables.PRESENCE:
-            if message["time"] and message["user"]["account_name"] in account_names:
-                SERVER_LOGGER.debug("Response 200")
-                return {"response": "200",
-                        "time": timestamp}
+            if sender not in accounts:
+                accounts[sender] = client
+                client.send(encode_message({"response": "200",
+                                            "time": time()}))
+                SERVER_LOGGER.debug(f"Регистрация нового пользователя {sender}")
 
-            elif message["time"] and message["user"]["account_name"] not in account_names:
-                SERVER_LOGGER.warning("Response 404")
-                return {"response": "404",
-                        "time": timestamp,
-                        "alert": "Пользователь/чат отсутствует на сервере"}
+            else:
+                client.send(encode_message({"response": "400",
+                                            "time": time(),
+                                            "alert": f"Пользователь {sender} уже существует!"}))
+                SERVER_LOGGER.warning(f"Response 400, Пользователь {sender} уже существует!")
 
-        elif message["action"] == variables.MSG:
-            if message["time"] and message["message"] and message["from"] == variables.SENDER_ACCOUNT_NAME \
-                    and message["to"] == "#all":
-                SERVER_LOGGER.debug("Response 200")
-                return message
-
+        elif message["action"] == 'msg':
+            sender = message["from"]
+            destination = message["to"]
+            if destination not in accounts:
+                raise UserWarning
+            else:
+                SERVER_LOGGER.debug(f"Получено сообщение от {sender} к {destination}")
+                messages.append(tuple((sender, message)))
         else:
             raise KeyError
 
+    except UserWarning:
+        SERVER_LOGGER.warning("Response 404")
+        client.send(encode_message({"response": "404",
+                                    "time": time(),
+                                    "alert": "Получатель не в сети, или не зарегистрирован!"}))
     except (KeyError, TypeError):
         SERVER_LOGGER.warning("Response 400")
-        return {"response": "400",
-                "time": timestamp,
-                "alert": "Неправильный запрос/JSON-объект"}
-
-    return message
+        client.send(encode_message({"response": "400",
+                                    "time": time(),
+                                    "alert": "Неправильный запрос/JSON-объект"}))
+    return
 
 
 def main():
@@ -89,7 +100,7 @@ def main():
         SERVER_LOGGER.critical("Значение <port> должно быть числом, в диапазоне с 1024 по 65535")
         sys.exit(1)
 
-    SERVER_LOGGER.debug(f"Запуск сервера c адрессом: {address}, портом: {port}")
+    SERVER_LOGGER.debug(f"Запуск сервера c адресом: {address}, портом: {port}")
 
     s = socket(AF_INET, SOCK_STREAM)
     s.bind((address, port))
@@ -98,6 +109,7 @@ def main():
 
     all_clients = []
     messages = []
+    accounts = {}
 
     while True:
         try:
@@ -121,11 +133,13 @@ def main():
             if receive_lst:
                 for recv_client in receive_lst:
                     try:
-                        SERVER_LOGGER.info(f"Получено сообщение от клиента {recv_client.getpeername()}")
+                        peername = recv_client.getpeername()
+                        SERVER_LOGGER.info(f"Получено сообщение от клиента {peername}")
                         received_data = recv_client.recv(variables.MAX_PACKAGE_LENGTH)
                         received_msg = decode_data(received_data)
                         print(f"Получил сообщение {received_msg}")
-                        messages.append(message_handler(received_msg))
+                        # messages.append(message_handler(received_msg))
+                        message_handler(received_msg, recv_client, messages, accounts)
                     except:
                         SERVER_LOGGER.info(f"Клиент {recv_client.getpeername()} отключился")
                         all_clients.remove(recv_client)
